f72e8e3d5b96ffc014480cd113e3e9c0803e31e2
suman naik <sumann14389@gmail.com>
2023-12-22 01:30:54 +0530

    commit 2

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -5,6 +5,9 @@ import twoSrc from "../Images/two.png"
 import threeSrc from "../Images/three.png"
 import fourSrc from "../Images/four.png"
 import fiveSrc from "../Images/five.png"
+import sixSrc from "../Images/six.png"
+import sevenSrc from "../Images/seven.png"
+import eightSrc from "../Images/eight.png";
 // import { Carousel } from '@trendyol-js/react-carousel';
 import Carousel from 'react-elastic-carousel'
 
@@ -18,12 +21,16 @@ const Photos = () => {
     console.log("photos");
 
     return <div className="photos-container">
-        <Carousel itemsToShow={2}>
+        <Carousel itemsToShow={2} className="carousel-class">
             <img src={oneSrc} />
             <img src={twoSrc} />
             <img src={threeSrc} />
             <img src={fourSrc} />
             <img src={fiveSrc} />
+            <img src={sixSrc} />
+            <img src={sevenSrc} />
+            <img src={eightSrc} />
+
         </Carousel>
     </div>
 }
